b64e257387820a1d7c10dce87f44ad44f6fedaa7
vecxoz <vecxoz@gmail.com>
2024-09-18 16:15:59 +0300

    Update 2. Lover coverage

--- pack/core.py
+++ pack/core.py
@@ -7,3 +7,11 @@ def circumference(radius):
     if not isinstance(radius, (int, float)) or radius <= 0:
         raise ValueError('Radius must be positive non-zero number (%s was given)' % str(radius))
     return 2 * np.pi * radius
+
+def circumference_2(diameter):
+    """
+    Calculate the circumference i.e. perimeter of a circle
+    """
+    if not isinstance(diameter, (int, float)) or diameter <= 0:
+        raise ValueError('Diameter must be positive non-zero number (%s was given)' % str(diameter))
+    return np.pi * diameter
